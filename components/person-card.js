8e55dfc75839af14f2d548438f0641101ca66da9
chtinahow <chtinahow@gmail.com>
2025-11-16 22:22:00 -0500

    Add person card with content
    Co-authored-by: Jesse Jurman <j.r.jurman@gmail.com>

--- components/person-card.js
+++ components/person-card.js
@@ -1,7 +1,20 @@
 const personCardStyleSheet = new CSSStyleSheet();
 personCardStyleSheet.replaceSync(/*css*/ `
 	:host {
+		display: block;
+	}
+	
+	person-card-container {
+		display: flex;
+		flex-direction: column;
+		gap: 0.2em;
+		aspect-ratio: 1 / 1;
+		width: 100px;
+		align-items: center;
+	}
 
+	wa-avatar {
+		--size: 64px;
 	}
 `);
 
@@ -11,6 +24,18 @@ class PersonCard extends HTMLElement {
 
 		this.attachShadow({ mode: 'open' });
 		this.shadowRoot.adoptedStyleSheets = [personCardStyleSheet];
+	}
+
+	connectedCallback() {
+		this.shadowRoot.innerHTML = /*html*/ `
+			<person-card-container>
+				<wa-avatar initials="TH" label="Avatar with initials: TH"></wa-avatar>
+				<span>Tina Howard</span>
+				<wa-tag size="small" pill>
+					<wa-format-date month="long" day="numeric" year="numeric" date="2020-07-15T09:17:00-04:00"></wa-format-date>
+				</wa-tag>
+			</person-card-container>
+		`;
 	}
 }
 
